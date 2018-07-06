0062285a9ab185e6ac9bae01a6aeaacedfac2adc
= <=>
2018-07-05 21:34:03 -0500

    second commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,78 @@
+  // Adding click event listen listener to all buttons
+  $("button").on("click", function() {
+    // Grabbing and storing the data-team property value from the button
+    var team = $(this).attr("data-team");
+
+    // Constructing a queryURL using the team name
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      team + "&api_key=dc6zaTOxFJmzC&limit=5";
+
+    // Performing an AJAX request with the queryURL
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After data comes back from the request
+      .then(function(response) {
+        console.log(queryURL);
+
+        console.log(response);
+        // storing the data from the AJAX request in the results variable
+        var results = response.data;
+
+        // Looping through each result item
+        for (var i = 0; i < results.length; i++) {
+
+          // Creating and storing a div tag
+          var teamDiv = $("<div>");
+
+          // Creating a paragraph tag with the result GIF's title
+          var p = $("<p>").text("Gif Title: " + results[i].title);
+
+          // Creating and storing an image tag
+          var teamImage = $("<img>");
+
+          // Setting the src attribute of the image to a property pulled off the result item
+          teamImage.attr("src", results[i].images.fixed_height.url);
+
+
+
+          teamImage.attr("data-state", results[i].images.fixed_width_small_still.url);
+          teamImage.attr("data-state", results[i].images.fixed_width_still.url);
+
+
+
+
+          $(".gif").on("click", function()
+          {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+
+            if (state === "still")
+            {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            }
+
+            else
+            {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
+
+
+
+          // Appending the paragraph and image tag to the teamDiv
+          teamDiv.append(p);
+          teamDiv.append(teamImage);
+
+          // Prependng the teamDiv to the HTML page in the "#gifs-appear-here" div
+          $("#gifs-appear-here").prepend(teamDiv);
+        }
+      });
+  });
